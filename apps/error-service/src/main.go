3c05948d76e4c9df89d077a91320be0ff2cd9480
David Borrego <dborrego@redhat.com>
2022-10-19 22:42:19 +0200

    1

--- apps/error-service/src/main.go
+++ apps/error-service/src/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"error-service/interfaces"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
@@ -26,7 +27,7 @@ func main() {
 	router.SetTrustedProxies(nil)
 	router.Use(CORS())
 
-	v1 := router.Group("/api/v1", cors.Default())
+	v1 := router.Group("/api/v1", CORS())
 	{
 		h := interfaces.HealthcheckHandler{}
 		v1.GET("/health", h.HealthcheckGetHandler())

13b3b24f4f86e5c0e4754dad48e29abaecd74615
David Borrego <dborrego@redhat.com>
2022-10-21 12:17:59 +0200

    health

--- apps/film-storage/src/interfaces/HealthcheckHandler.go
+++ apps/film-storage/src/interfaces/HealthcheckHandler.go
@@ -1,19 +1,32 @@
 package interfaces
 
 import (
+	"film-storage/domain/dto"
+	"film-storage/domain/model"
+	"log"
 	"net/http"
-	"ta-candle-store/domain/dto"
 
 	"github.com/gin-gonic/gin"
 )
 
 type HealthcheckHandler struct {
+	filmDao *model.FilmRepository
+}
+
+func NewHealthcheckHandler(dao *model.FilmRepository) *HealthcheckHandler {
+	return &HealthcheckHandler{filmDao: dao}
 }
 
 func (handler *HealthcheckHandler) HealthcheckGetHandler() func(c *gin.Context) {
 	return func(c *gin.Context) {
 		h := dto.Health{}
-		h.Status = dto.HealhStatusUp
+		err := handler.filmDao.HealthCheck()
+		if err != nil {
+			log.Printf("%s", err)
+			h.Status = dto.HealhStatusDown
+		} else {
+			h.Status = dto.HealhStatusUp
+		}
 		c.JSON(http.StatusOK, h)
 	}
 }

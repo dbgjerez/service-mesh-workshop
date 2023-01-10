e688b10dadf14955d0fbec29e7f968d5d1983252
David Borrego <dborrego@redhat.com>
2023-01-10 14:49:06 +0100

    store envs

--- apps/film-storage/src/domain/model/Store.go
+++ apps/film-storage/src/domain/model/Store.go
@@ -3,17 +3,25 @@ package model
 import (
 	"encoding/json"
 	"errors"
+	"film-storage/utils"
 	"fmt"
 	"io/ioutil"
 	"os"
+)
+
+const (
+	ENV_CONFIG_JSON_LOCATION               string = "ENV_CONFIG_JSON_LOCATION"
+	ENV_CONFIG_JSON_LOCATION_DEFAULT_VALUE string = "./data.json"
 )
 
 type Store struct {
 	Path string
 }
 
-func NewStore(path string) *Store {
-	return &Store{Path: "./" + path}
+func NewStore() *Store {
+	path := utils.GetEnv(ENV_CONFIG_JSON_LOCATION,
+		ENV_CONFIG_JSON_LOCATION_DEFAULT_VALUE)
+	return &Store{Path: path}
 }
 
 func (store *Store) Exists() bool {

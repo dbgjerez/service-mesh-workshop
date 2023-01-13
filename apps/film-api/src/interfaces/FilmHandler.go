3ddf172695c59c9cdda984cc56285428a71cde3e
David Borrego <dborrego@redhat.com>
2023-01-13 20:14:46 +0100

    api

--- apps/film-api/src/interfaces/FilmHandler.go
+++ apps/film-api/src/interfaces/FilmHandler.go
@@ -1,1 +1,42 @@
 package interfaces
+
+import (
+	"film-api/services"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	TOKEN_USER = "user"
+)
+
+type FilmHandler struct {
+	fService *services.FilmService
+	tService *services.TokenService
+}
+
+func NewFilmHandler(fService *services.FilmService, tService *services.TokenService) *FilmHandler {
+	return &FilmHandler{
+		fService: fService,
+		tService: tService,
+	}
+}
+
+func (handler *FilmHandler) GetFilmsByUserHandler() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		jwtToken := c.GetHeader("Authorization")
+		username, err := handler.tService.GetClaim(jwtToken, TOKEN_USER)
+		if err != nil {
+			c.JSON(http.StatusForbidden, fmt.Sprintf("%v", err))
+		} else {
+			films, err := handler.fService.FindFilmsByUser(username)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, err)
+			} else {
+				c.JSON(http.StatusOK, films)
+			}
+		}
+	}
+}
